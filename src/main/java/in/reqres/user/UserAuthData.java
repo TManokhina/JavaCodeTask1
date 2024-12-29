54abefe0c94a7bb6910d53345fc122a27eb49ccd
TManokhi <m@nokhi.ru>
2024-12-29 23:23:04 +0300

    Create UserAuthData for deserialization response body

---
+++ src/main/java/in/reqres/user/UserAuthData.java
@@ -0,0 +1,22 @@
+package in.reqres.user;
+
+public class UserAuthData {
+    private Integer id;
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
